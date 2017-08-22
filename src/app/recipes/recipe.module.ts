cbf4e34a5fdb184927574e25811f609607d4f41e
Manlio Marchica <manlio.marchica@live.it>
2017-08-22 21:40:07 +0200

    Lezione 153 recipe edit

---
+++ src/app/recipes/recipe.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Route, RouterModule } from '@angular/router';
+import { RecipesComponent } from './recipes.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+// import { RecipeService } from './recipe.service';
+
+const childRoutes: Route[] = [
+    {path: '', component: RecipesComponent, children: [
+        {path: '', component: RecipeStartComponent},
+        {path: 'new', component: RecipeEditComponent },
+        {path: ':id', component: RecipeDetailComponent },
+        {path: ':id/edit', component: RecipeEditComponent }
+    ]}
+  ];
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeDetailComponent,
+        RecipeListComponent,
+        RecipeItemComponent,
+        RecipeStartComponent,
+        RecipeEditComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(childRoutes)
+    ]// ,
+    // providers: [RecipeService] spostato nel componente recipes.component.ts
+})
+export class RecipeModule {}
